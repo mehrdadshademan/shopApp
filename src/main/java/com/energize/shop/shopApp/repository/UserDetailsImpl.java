93942a398e87803b90535a59acd251897d4ec043
Wir <fff>
2021-06-28 00:56:53 +0430

    complete when get token

--- src/main/java/com/energize/shop/shopApp/repository/UserDetailsImpl.java
+++ src/main/java/com/energize/shop/shopApp/repository/UserDetailsImpl.java
@@ -8,18 +8,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class UserDetailsImpl  implements UserDetails {
-    private Long id ;
+public class UserDetailsImpl implements UserDetails {
+     private Long id;
     private String username;
     private String email;
     @JsonIgnore
     private String password;
 
-    private Collection<? extends GrantedAuthority > authorities;
+    private Collection<? extends GrantedAuthority> authorities;
 
-    public UserDetailsImpl(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+    public UserDetailsImpl(Long id, String username, String email, String password,
+                           Collection<? extends GrantedAuthority> authorities) {
         this.id = id;
         this.username = username;
         this.email = email;
@@ -27,34 +29,30 @@ public class UserDetailsImpl  implements UserDetails {
         this.authorities = authorities;
     }
 
-public static UserDetailsImpl build(User user)
-{
-    List<GrantedAuthority> authorities = user.getRoles().stream().map(r->new SimpleGrantedAuthority(r.getName().name()))
-            .collect(Collectors.toList());
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
 
-    return new UserDetailsImpl(user.getId() , user.getUsername(),user.getEmail(),user.getPassword() , authorities);
-}
-
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
@@ -85,5 +83,15 @@ public static UserDetailsImpl build(User user)
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
     }
 }
