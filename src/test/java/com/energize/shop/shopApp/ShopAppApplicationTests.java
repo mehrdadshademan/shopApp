4c4bf8279017fc73806253a24af0d48954b4de74
mehrdadshademan <eng.mehrdad@gmaol.com>
2021-06-27 18:40:32 +0430

    add docker

---
+++ src/test/java/com/energize/shop/shopApp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energize.shop.shopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
